4e01ec761afd3c57d5bc1574dd30a8b0e3a3b4ab
thomascsd <thomascsd808@gmail.com>
2014-06-08 00:43:14 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#MBGallery#
+
+  顯示照片的相簿
+
+  Node.js,Backbone.js,Bootstrap.js
--- server.js
+++ server.js
@@ -6,24 +6,28 @@ var path = require('path');
 var cons = require('consolidate');
 var express = require('express');
 var routes = require('./routes/');
-
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var log = require('morgan');
 var app = express();
 
-app.configure(function () {
-    app.engine('html', cons.handlebars);
-    app.set('view engine', 'html');
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.static(path.join(__dirname, 'bower_components')));
-    app.use(express.static(path.join(__dirname, 'public')));
-});
+app.engine('html', cons.handlebars);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 3000);
+app.use(log('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', routes.home.index);
+//Route
+var apiRouter = express.Router();
+apiRouter.get('/photos/:folderID', routes.photo.getPhotos);
+apiRouter.post('/photos', routes.photo.upload);
+app.use('/api', apiRouter);
 
-app.get('/api/photos/:folderID', routes.photo.getPhotos);
-app.post('/api/photos', routes.photo.upload);
+app.route('/')
+.get(routes.home.index);
 
 app.listen(app.get('port'));
