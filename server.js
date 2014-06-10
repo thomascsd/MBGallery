3653f47cfcf4720cb75c9a864e046c2ff7adc30f
thomascsd <thomascsd808@gmail.com>
2014-06-10 16:49:38 +0800

    Add Upload

--- server.js
+++ server.js
@@ -5,10 +5,13 @@
 var path = require('path');
 var cons = require('consolidate');
 var express = require('express');
-var routes = require('./routes/');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var log = require('morgan');
+var multiParty = require('connect-multiparty');
+var multiPartyMiddleware = multiParty({ uploadDir: path.join(__dirname, 'upload') });
+var routes = require('./routes/');
+
 var app = express();
 
 app.engine('html', cons.handlebars);
@@ -21,14 +24,15 @@ app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Route
-var apiRouter = express.Router();
-apiRouter.get('/photos/:folderID', routes.photo.getPhotos);
-apiRouter.post('/photos', routes.photo.upload);
-app.use('/api', apiRouter);
-
 //Default Route
 app.route('/')
-.get(routes.home.index);
+    .get(routes.home.index);
+
+//API
+var apiRouter = express.Router();
+apiRouter.get('/photos/:folderID', routes.photo.getPhotos);
+apiRouter.post('/photos', multiPartyMiddleware, routes.photo.upload);
+app.use('/api', apiRouter);
+
 
 app.listen(app.get('port'));
